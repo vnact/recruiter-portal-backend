2ca26c3fb69ca471143937dddfd33ce718a3519f
DC <cuong.nl5.kma@gmail.com>
2022-09-18 22:51:42 +0700

    fix: update lint staged

--- lint-staged.js
+++ lint-staged.js
@@ -1,4 +1,4 @@
 module.exports = {
-  './@(apps|libraries)/**/*.+(js|ts|tsx)': ['yarn lint:fix', 'yarn lint'],
-  './@(apps|libraries)/**/*.{js,jsx,ts,tsx,json,css,scss,md}': ['yarn format:write', 'yarn format:check'],
+  './@(apps|libraries)/**/*.+(js|ts|tsx)': ['yarn lint'],
+  './@(apps|libraries)/**/*.{js,jsx,ts,tsx,json,css,scss,md}': ['yarn format:check'],
 }
