4a5776637b226a1e679cf8da64511fcfd7d1ea7f
DC <cuong.nl5.kma@gmail.com>
2022-09-28 21:43:35 +0700

    feat: seed skill

---
+++ apps/recruiter-cms/src/databases/seeds/1664365455290-SeedSkill.ts
@@ -0,0 +1,31 @@
+import { SkillEntity } from '@modules/skills/entities/skill.entity';
+import { In, MigrationInterface, QueryRunner } from 'typeorm';
+
+const skills: string[] = [
+  'Linux',
+  'SQL',
+  'HTML',
+  'Javascript',
+  'CSS',
+  'Go (Programming Language)',
+  'PHP',
+  'Python',
+  'Talent Management',
+  'English',
+];
+
+export class SeedSkill1664365455290 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await Promise.all(
+      skills.map((jobName) =>
+        queryRunner.manager.insert(SkillEntity, { name: jobName }),
+      ),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager.delete(SkillEntity, {
+      name: In(skills),
+    });
+  }
+}
