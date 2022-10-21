8b1f2392b00dbd068a6a6bec0d65362130ed0f11
Nguyễn Lam Sơn <67233512+lamsonkma@users.noreply.github.com>
2022-10-21 08:58:45 -0700

    fix: reslove delete user skill (#11)
    Co-authored-by: Nguyen Lam Son <sonnl.bhsoft@gmail.com>

--- apps/recruiter-cms/src/modules/users/commands/index.ts
+++ apps/recruiter-cms/src/modules/users/commands/index.ts
@@ -1,9 +1,9 @@
 import { CreateUserSkillHandler } from './create-user-skill.command';
 import { CreateUserCommandHandler } from './create-user.command';
-import { DeleteUserUserSkillHandler } from './delete-user-skill.command';
+import { DeleteUserSkillCommandHandler } from './delete-user-skill.command';
 
 export const UserCommandHandlers = [
   CreateUserCommandHandler,
   CreateUserSkillHandler,
-  DeleteUserUserSkillHandler,
+  DeleteUserSkillCommandHandler,
 ];
