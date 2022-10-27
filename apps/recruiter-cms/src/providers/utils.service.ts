25f3545ca3a9cc3be9b49ae612ca5d9cc39e3f33
Trần Đức Cường <36887165+Notekunn@users.noreply.github.com>
2022-10-28 01:00:06 +0700

    feat: add seed job (#18)

--- apps/recruiter-cms/src/providers/utils.service.ts
+++ apps/recruiter-cms/src/providers/utils.service.ts
@@ -45,4 +45,37 @@ export class UtilService {
       ? (value as unknown as T)
       : undefined;
   }
+
+  static getRandomInRange(from: number, to: number, fixed: number) {
+    const randomNumber = Math.random() * (to - from) + from;
+    const fixedNumber = +randomNumber.toFixed(fixed);
+    return fixedNumber;
+  }
+
+  static getRandom(from: number, to: number) {
+    return Math.floor(Math.random() * (to - from)) + from;
+  }
+
+  static pickOne<T>(arr: T[]): T {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  static pickMany<T>(arr: T[], take?: number): T[] {
+    const items = arr.length;
+    if (!take) {
+      take = Math.floor(Math.random() * items);
+    }
+
+    if (take >= items) return arr;
+
+    const randomIndex: number[] = [];
+    while (randomIndex.length < take) {
+      const r = Math.floor(Math.random() * items);
+      if (!randomIndex.includes(r)) {
+        randomIndex.push(r);
+      }
+    }
+
+    return randomIndex.map((index) => arr[index]).sort();
+  }
 }
