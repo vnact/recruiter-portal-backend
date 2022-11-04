a2e703f7d8ea1d8122996e90353f45e120a13b57
Nguyen Lam Son <sonnl.bhsoft@gmail.com>
2022-11-04 17:10:11 +0700

    feat: update api company

--- apps/recruiter-cms/src/modules/companies/controllers/company.controller.ts
+++ apps/recruiter-cms/src/modules/companies/controllers/company.controller.ts
@@ -1,5 +1,5 @@
 import { PaginationDto } from '@common/dto/pagination.dto';
-import { Body, Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 import { ApiTags } from '@nestjs/swagger';
 import { GetAllCompanyQuery } from '../queries/get-all-company.query';
@@ -9,7 +9,7 @@ import { GetAllCompanyQuery } from '../queries/get-all-company.query';
 export class CompanyController {
   constructor(private readonly queryBus: QueryBus) {}
   @Get()
-  getAll(@Body() dto: PaginationDto) {
+  getAll(@Query() dto: PaginationDto) {
     return this.queryBus.execute(new GetAllCompanyQuery(dto));
   }
 }
